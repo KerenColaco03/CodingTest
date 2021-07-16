0e73fb5c735fa82086616c9b7fbd96fe0fe3f60f
KerenColaco03 <60647277+KerenColaco03@users.noreply.github.com>
2021-07-16 16:11:38 +0100

    Modified js

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//CHECKS TO SEE IF BOTH PASSWORDS MATCH
+const checkConfirmation = (ev) => {
+ if (myPassword.value != myConfirmation.value) {
+  myConfirmation.setCustomValidity("Wait. What? This doesn't match the password field!");
+ } else {
+  myConfirmation.setCustomValidity('');
+ }
+};
+
+myConfirmation.addEventListener('input', checkConfirmation);
+myPassword.addEventListener('input', checkConfirmation);
+
+
+
+//DECLARING THE VARIABLE
+let user = {}
+signin.addEventListener('submit', ev => {
+
+  alert("Thank you for signing up")
+
+//ASSIGNING VALUE TO THE PROPERTY
+    user.firstName = myFirstName.value;
+    user.lastName = myLastName.value;
+    user.email = myEmail.value;
+    user.phoneNumber = myPhoneNo.value;
+    user.currentJobTitle = myCurrentJobTitle.value;
+    user.password = myPassword.value;
+    user.confirmPassword = myConfirmation.value;
+    user.howDidYouFindAboutUs = myOptions.value;
+
+console.log(user)
+
+
+ signin.reset();
+ ev.preventDefault();
+});
